4976f015e2b347596e00ec104584cad2640ad422
Ryeyao <rye.y.cn@gmail.com>
2013-06-01 15:57:19 +0800

    Added some files
    Signed-off-by: Ryeyao <rye.y.cn@gmail.com>

---
+++ GWF_Services/Entities/GWF_User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Text.RegularExpressions;
+
+using GWF_WebServices.Entities.UserEntities;
+
+namespace GWF_WebServices.Models
+{
+    public partial class GWF_User
+    {
+        public GWF_User()
+        {
+            currentState = new OfflineState();
+        }
+
+        public UserState currentState
+        {
+            get;
+            set;
+        }
+    }
+}
